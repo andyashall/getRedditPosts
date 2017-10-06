c0f17e22599d3b7e4202b35578bd5e9a59e4cc5a
andy <andy@ashall.co>
2017-10-06 15:31:11 +0100

    now skips blank comments

--- src/Lib.hs
+++ src/Lib.hs
@@ -44,10 +44,10 @@ getComments :: String -> String -> IO ()
 getComments sub id = do
     let url = "https://www.reddit.com/r/" Prelude.++ sub Prelude.++ "/comments/" Prelude.++ id Prelude.++ ".json"
     r <- get url
-    printCom 0 r
+    parseComments 0 r
 
-printCom :: Int -> Response ByteString -> IO ()
-printCom n r
+parseComments :: Int -> Response ByteString -> IO ()
+parseComments n r
     | n >= 25 = askForSub
     | n < 25 = do
         let f1 = "data" :: T.Text
@@ -55,8 +55,8 @@ printCom n r
         let f3 = "body" :: T.Text
         let getPosts = responseBody . nth 1 . key f1 . key f2
         let p = r ^? getPosts . nth n . key f1 . key f3 . _String
-        putStrLn $ id (show (n+1)) Prelude.++ ". " Prelude.++ (strin p)
-        printCom (n+1) r
+        let s = (strin p)
+        printComments n r s
 
 printEm :: String -> Int -> Response ByteString -> IO ()
 printEm sub n r
@@ -73,3 +73,10 @@ printEm sub n r
 strin :: Maybe T.Text -> String
 strin Nothing = ""
 strin (Just x) = (T.unpack x)
+
+printComments :: Int -> Response ByteString -> String -> IO()
+printComments n r s
+    | s == "" = askForSub
+    | otherwise = do
+        putStrLn $ id (show (n+1)) Prelude.++ ". " Prelude.++ s
+        parseComments (n+1) r
