d5ac0529df963462d4baae2cbf32d6d494c269f5
andy <andy@ashall.co>
2017-10-06 11:08:43 +0100

    now gets 25 comments from the selected post

--- src/Lib.hs
+++ src/Lib.hs
@@ -32,32 +32,44 @@ openPost sub r = do
     let f2 = "children" :: T.Text
     let f3 = "id" :: T.Text
     let getPosts = responseBody . key f1 . key f2
-    let id = r ^? getPosts . nth n . key f1 . key f3 . _String
+    let id = r ^? getPosts . nth (n-1) . key f1 . key f3 . _String
     getComments sub (strin id)
 
 getPosts :: String -> IO ()
 getPosts sub = do
     let url = "https://www.reddit.com/r/" Prelude.++ sub Prelude.++ ".json"
     r <- get url
-    printEm ("title" :: T.Text) sub 0 r
+    printEm sub 0 r
 
 getComments :: String -> String -> IO ()
 getComments sub id = do
     let url = "https://www.reddit.com/r/" Prelude.++ sub Prelude.++ "/comments/" Prelude.++ id Prelude.++ ".json"
     r <- get url
-    printEm ("selftext" :: T.Text) sub 0 r
+    printCom 0 r
 
-printEm :: T.Text -> String -> Int -> Response ByteString -> IO ()
-printEm ty sub n r
+printCom :: Int -> Response ByteString -> IO ()
+printCom n r
+    | n >= 25 = askForSub
+    | n < 25 = do
+        let f1 = "data" :: T.Text
+        let f2 = "children" :: T.Text
+        let f3 = "body" :: T.Text
+        let getPosts = responseBody . nth 1 . key f1 . key f2
+        let p = r ^? getPosts . nth n . key f1 . key f3 . _String
+        print $ show (n+1) Prelude.++ ". " Prelude.++ (strin p)
+        printCom (n+1) r
+
+printEm :: String -> Int -> Response ByteString -> IO ()
+printEm sub n r
     | n >= 25 = openPost sub r
     | n < 25 = do
         let f1 = "data" :: T.Text
         let f2 = "children" :: T.Text
-        -- let f3 = ty :: T.Text
+        let f3 = "title" :: T.Text
         let getPosts = responseBody . key f1 . key f2
-        let p = r ^? getPosts . nth n . key f1 . key ty . _String
+        let p = r ^? getPosts . nth n . key f1 . key f3 . _String
         print $ show (n+1) Prelude.++ ". " Prelude.++ (strin p)
-        printEm ty sub (n+1) r
+        printEm sub (n+1) r
 
 strin :: Maybe T.Text -> String
 strin Nothing = ""
